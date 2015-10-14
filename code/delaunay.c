c9e43eb83f1ec07f440eeb2f664a85a526eeefd0
Yunjoo Park <yunjoopark12@gmail.com>
2015-10-14 22:00:29 +0900

    first commit

--- code/delaunay.c
+++ code/delaunay.c
@@ -18,6 +18,7 @@ extern tTetra tetras;
 /*-------------------------------------------------------------------*/
 void	Delaunay( void )
 {
+	printf("delaunay\n");
 	// Use qhull to compute Delaunay triangulation of the points.
 	// put results (vertices, edges, faces, and tetrahedra)
 	// in the global structures above
@@ -40,6 +41,117 @@ void	Delaunay( void )
 	//to fill the teta: get vertices of facet and loop through each vertex
 	//use FOREACHvertex_()
 	//
+
+	tVertex  ptr_v;
+	tVertex * all_v = NULL;
+	int vsize = 0;
+	int id = 0;
+
+	//global varibles for qhull
+	static char * options = (char *)"delaunay QJ Pp";
+	int curlong, totlong;
+	coordT * pt = NULL;
+	facetT *facet = NULL;
+	vertexT *vertex = NULL;
+	vertexT **vertexp = NULL;
+	facetT *neighbor, **neighborp;
+	int vid = 0;
+	//tFace  face;
+
+	double pt4;
+	tTetra tetra;
+
+	//count number of points
+	ptr_v = vertices;
+	do {
+		vsize++;
+		ptr_v = ptr_v->next;
+	} while (ptr_v != vertices);
+
+	//allocate memory
+	pt = (coordT*)calloc(vsize * 4, sizeof(coordT)); //each point will have four coord
+	all_v = (tVertex*)calloc(vsize, sizeof(tVertex));
+	assert(pt && all_v);
+
+	//copy points
+	ptr_v = vertices;
+	do {
+		pt4 = ptr_v->v[0] * ptr_v->v[0] + ptr_v->v[1] * ptr_v->v[1] + ptr_v->v[2] * ptr_v->v[2];
+		pt[id++] = ptr_v->v[0];
+		pt[id++] = ptr_v->v[1];
+		pt[id++] = ptr_v->v[2];
+		pt[id++] = pt4;
+		all_v[ptr_v->vnum] = ptr_v;
+		ptr_v = ptr_v->next;
+	} while (ptr_v != vertices);
+
+	//using qhull
+
+	qh_init_A(stdin, stdout, stderr, 0, NULL);
+
+	//qh DELAUNAY= True;     /* 'd'   */
+	//qh SCALElast= True;    /* 'Qbb' */
+	//qh KEEPcoplanar= True; /* 'Qc', to keep coplanars in 'p' */
+
+	qh_initflags(options);
+	qh_init_B(pt, vsize, 4, false);
+	qh_qhull();
+	qh_check_output();
+
+	//loop through all faces
+	FORALLfacets
+	{
+		tetra = MakeNullTetra();	// make a tetra
+
+		//get vertices of facet
+		//loop through each vertex
+		vid = 0;
+		FOREACHvertex_(facet->vertices)
+		{
+			//get the id of the vertex
+			tetra->vertex[vid++] = all_v[qh_pointid(vertex->point)];
+		}
+
+		//to fill the teta: get vertices of facet and loop through each vertex
+		//use FOREACHvertex_()
+		FOREACHneighbor_(facet)
+		{
+			if (facet < neighbor) {
+				tVertex vertices[4];
+				vid = 0;
+				FOREACHvertex_(neighbor->vertices)
+				{
+					//get vertex
+					vertices[vid++] = all_v[qh_pointid(vertex->point)];
+				}	
+				if (facet->normal[3] < 0) {
+					tetra->face[0] = MakeFace(tetra->vertex[0], tetra->vertex[1], tetra->vertex[2], NULL);
+					tetra->face[1] = MakeFace(tetra->vertex[0], tetra->vertex[1], tetra->vertex[3], NULL);
+					tetra->face[2] = MakeFace(tetra->vertex[0], tetra->vertex[2], tetra->vertex[3], NULL);
+					tetra->face[3] = MakeFace(tetra->vertex[1], tetra->vertex[2], tetra->vertex[3], NULL);
+
+				}
+			}
+		}//FOREACHneighbor_
+
+		
+
+		//QHullCreateEdge(tetra->vertex, vertices);
+
+
+	}
+
+	//not used
+	free(pt);
+	free(all_v);
+	pt = NULL;
+	all_v = NULL;
+
+	//free mem
+	qh_freeqhull(!qh_ALL);
+	qh_memfreeshort(&curlong, &totlong);
+
+
 }
 
 
